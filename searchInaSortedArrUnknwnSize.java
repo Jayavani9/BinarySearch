d3e24b7a8d2fecd4e33eb809f8b920353f3bcc23
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-11-06 13:07:23 -0800

    Update searchInaSortedArrUnknwnSize.java

--- searchInaSortedArrUnknwnSize.java
+++ searchInaSortedArrUnknwnSize.java
@@ -2,6 +2,7 @@ class srchunkn
 {
     public int search(ArrayReader reader, int target)
     {
+        //Tc: O(logn) Sc: O(1)
         int l = 0;
         int h = 1;
         while(reader.get(h) < target){
@@ -11,8 +12,8 @@ class srchunkn
         while(l <= h)
         {
             int mid = l + (h -l)/2;
-            if(reader.get(mid) == target) return mid;
-            else if (target < reader.get(mid)) h = mid-1;
+            if(reader.get(mid) == target) return mid; //Recursion can also be done recursively here instead of an iterative approach.
+            else if (target < reader.get(mid)) h = mid-1; // But Recursion using recursive stack takes extra space for the stack and is not advisable.
             else l = mid +1;
         }
         return -1;
